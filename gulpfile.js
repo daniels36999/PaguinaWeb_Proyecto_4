c616e832e92b7673e192207af6af6dfae47c96e9
daniels36999 <dyautibug.fie@unach.edu.ec>
2024-02-07 16:24:41 -0500

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,18 @@
 
 
 //SRC=IDENTIFICA  DEST=ALMACENA
-const{src, dest, watch} = require("gulp");
-const sass = require('gulp-sass')(require('sass'));
-const plumber = require('gulp-plumber');
+const{src, dest, watch, parallel} = require("gulp");           //PROPIEDADES DE GULP
+const sass = require('gulp-sass')(require('sass'));  //SASS
+const plumber = require('gulp-plumber');             //AUTOMATIZAR
+
+//IMAGENES
+const cache = require('gulp-cache');
+const  imagemin = require('gulp-imagemin'); // LIB IMAGENES WEB
+const  webp = require('gulp-webp'); // LIB IMAGENES WEB
+const  avif = require('gulp-avif'); // LIB IMAGENES WEB
 
 
+//FUNCION PARA EJECUTAR SASS
 function css(callback){
 
     src('src/scss/**/*.scss')       //IDENTIFICAR EL ARCHIVO DE SASS - (ACTUALIZA MULTIPLES ARCHIVOS SCSS)
@@ -20,14 +27,75 @@ function css(callback){
     callback(); //INDICA AL GULP QUE FINALIZO
 }
 
-exports.css = css;
 
-function dev(callback){
+//FUNCION PARA COMPRIMIR IMAGENES
+function comprimirImagen(callback){
 
-    //watch('src/scss/app.scss',css)
-    watch('src/scss/**/*.scss',css)
+    const opciones = {
+        optimizationLevel: 3
+    };
+
+    src('src/img/**/*.{png,jpg}') //RUTA: ** TODAS las carpetas
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest('build/img'))
+
+    callback(); //INDICA AL GULP QUE FINALIZO
+}
+
+//FUNCION PARA CONVERTIR IMAGENES A WEBP
+function convertirWebp(callback){
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}') //RUTA: ** TODAS las carpetas
+    .pipe(webp(opciones))
+    .pipe(dest('build/img'));
+
+    callback(); //INDICA AL GULP QUE FINALIZO
+}
+
+//FUNCION PARA CONVERTIR IMAGENES A AVIF
+function convertirAvif(callback){
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}') //RUTA: ** TODAS las carpetas
+    .pipe(avif(opciones))
+    .pipe(dest('build/img'));
+
+    callback(); //INDICA AL GULP QUE FINALIZO
+}
+
+//EJECUTAR JAVASCRIPT
+function runJava(callback){
+
+    src('src/js/**/*.js')
+    .pipe(dest('build/js'));
+
     callback();
 }
 
+//AUTOMATIZA  SASS
+function dev(callback){
 
+    //watch('src/scss/app.scss',css)
+    watch('src/scss/**/*.scss',css);
+    watch('src/js/**/*.js',runJava);
+    callback();
+}
+
+exports.css = css;
+exports.runJava = runJava;
 exports.dev = dev;
+exports.convertirWebp = convertirWebp;
+exports.comprimirImagen = comprimirImagen;
+exports.convertirAvif = convertirAvif;
+
+//EJECUTA PARALELAMENTE 
+//exports.dev = parallel(comprimirImagen, convertirWebp, dev);
+//exports.comprimirImagen = parallel(comprimirImagen, convertirWebp, convertirAvif);
+
